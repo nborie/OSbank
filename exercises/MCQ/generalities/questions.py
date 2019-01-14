6c73b58a4ed9472c34ee5d143dae0f81601535e8
Nicolas Borie <nicolas.borie@u-pem.fr>
2019-01-14 12:15:15 +0100

    Add two first question in AMC format

--- exercises/MCQ/generalities/questions.py
+++ exercises/MCQ/generalities/questions.py
@@ -1,1 +1,41 @@
-Ceci est une première ligne
+* À quoi sert le système d'exploitation sur un ordinateur ?
+- Il met en place un régime d'oppression sur les micropuces
+  informatiques.
+- Il est responsable de charger le CSS dans les pages web HTML.
+- Il produit le bytecode Java à partir du code source Java.
+- Il recharge la pile des appels avec du courant alternatif.
+- Il permet de visualiser le débogage dans un Traceback Python.
+- Il détecte les erreurs de syntaxe dans un code source C.
+- Il est responsable de la détection des variables Python non
+  utilisées afin de prévoir leur libération.
++ Il gère les entrées/sorties et les interactions avec le nomde extérieur.
++ Il propose des interfaces de haut niveau avec les ressources matériels.
++ Il cache les spécificités matériels du hardware (avec des pilotes).
++ Il permet un partage judicieux des ressources entre les utilisateurs.
++ Il permet un partage judicieux des ressources entre les différents
+  programmes.
++ Il assure l'intégrité et la cohérence des données de la machine.
++ Il fait l'interface entre le matériel (hardware) et les logiciels
+  applicatifs (software).
+
+* Parmi les opérations informatiques suivantes, laquelle(lesquels)
+  sollicite(nt) le système d'exploitation ?
+- Changer la pile de carte mère alimentant la puce du BIOS.
+- Nettoyer les circuits imprimés et les dissipateurs de chaleurs avec
+  de l'air pulsé.
+- Changer une touche du clavier cassé.
+- Calculer la complexité d'un algorithme de tri.
+- Établir une bijection entre les nombres binaire sur 32 bits et les
+  entiers positifs plus petits que 4294967296.
+- Vérifier que les bases de données sont structurés en forme normale.
+- Utiliser une méthode diviser pour régner.
++ Lancer l'exécution d'un programme.
++ Sauvegarder un fichier.
++ Parcourir le contenu d'un répertoire.
++ Bouger le pointeur de la souris à l'écran.
++ Éteindre le système avant une décharge complète d'une batterie
+  Lithium.
++ Partager la puissance de calcul du processeur entre plusieurs
+  programmes exécutés en même temps.
++ Demander de la mémoire pérenne supplémentaire dans un programme C
+  (avec malloc).
