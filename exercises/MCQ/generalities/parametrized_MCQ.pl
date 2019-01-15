5d1f591b24db7e739991e4b17e16a191b739c768
www-data <www-data@pl.u-pem.fr>
2019-01-15 17:51:46 +0100

    new framework of automatic parametrized MCQ based on ACM syntax

---
+++ exercises/MCQ/generalities/parametrized_MCQ.pl
@@ -0,0 +1,14 @@
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+author=Nicolas Borie
+
+title=Généralités sur les systèmes d'exploitation
+tag=system
+
+template=/templates/parametrized_MCQ.pl
+
+@ /exercises/MCQ/generalities/questions.py
+data_from_file=questions.py
+
+number_question=5
+min_option=4
+max_option=8
