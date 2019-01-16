4b099aa9437a91eeb13dde59e32ff08a4475050a
www-data <www-data@pl.u-pem.fr>
2019-01-16 10:48:53 +0100

    tss tss tss

---
+++ exercises/MCQ/generalities/plqcm.pl
@@ -0,0 +1,12 @@
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+author=Nicolas Borie
+
+title=Généralités sur la Syntaxe pl
+tag=system
+
+template=/templates/qcm.pl
+
+@ /exercises/MCQ/generalities/plq.txt [questions.txt]
+data_from_file=questions.txt
+
+
