6ec78a52d1e8395174b45c545261484095453e26
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-03-24 10:13:41 +0100

    Mise en place d'un PLTP avec 2 QCM en 1

--- exercises/MCQ/donnees_binaire_MCQ.pl
+++ exercises/MCQ/donnees_binaire_MCQ.pl
@@ -1,15 +1,16 @@
 # Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
 author=Nicolas Borie
 
-title=Généralités sur les systèmes d'exploitation
+title=Données binaires dans nos ordinateurs modernes
 tag=system
 
 template=/templates/parametrized_MCQ.pl
 
-@ /exercises/MCQ/generalités_système.txt
-data_from_file=generalités_système.txt
+@ /exercises/MCQ/donnees_binaire.txt
+data_from_file=donnees_binaire.txt
 
 number_question=5
 min_option=4
 max_option=8
+
 
